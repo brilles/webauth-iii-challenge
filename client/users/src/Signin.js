032e1ba8b491dfbe7dc92f8bee3a0d825bfc7a83
Brian Illes <brianrilles@gmail.com>
2019-04-04 13:28:25 -0700

    user can register and login

--- client/users/src/Signin.js
+++ client/users/src/Signin.js
@@ -1,16 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default function Signin() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    signin({
+      username,
+      password
+    });
+    setUsername('');
+    setPassword('');
+  };
+
+  const signin = creds => {
+    axios
+      .post('http://localhost:5000/api/login', creds)
+      .then(res => console.log(res))
+      .catch(err => {
+        console.log(err);
+      });
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Sign In</h2>
-        <input type="text" name="username" placeholder="Username" />
-        <input type="password" name="password" placeholder="Pasword" />
-        <Link to="/users">
-          <button type="submit">Sign In</button>
-        </Link>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Pasword"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          required
+        />
+        {/* <Link to="/users"> */}
+        <button type="submit">Sign In</button>
+        {/* </Link> */}
       </form>
     </div>
   );
--- client/users/src/Signup.js
+++ client/users/src/Signup.js
@@ -1,17 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default function Signup() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [department, setDepartment] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    signup({
+      username,
+      password,
+      department
+    });
+    setUsername('');
+    setPassword('');
+    setDepartment('');
+  };
+
+  const signup = creds => {
+    axios
+      .post('http://localhost:5000/api/register', creds)
+      .then(res => console.log(res))
+      .catch(err => {
+        console.log(err);
+      });
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Sign Up</h2>
-        <input type="text" name="username" placeholder="Username" />
-        <input type="password" name="password" placeholder="Pasword" />
-        <input type="text" name="password" placeholder="Department" />
-        <Link to="/users">
-          <button type="submit">Sign Up</button>
-        </Link>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Pasword"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          required
+        />
+        <input
+          type="text"
+          name="password"
+          placeholder="Department"
+          value={department}
+          onChange={e => setDepartment(e.target.value)}
+        />
+        {/* <Link to="/users"> */}
+        <button type="submit">Sign Up</button>
+        {/* </Link> */}
       </form>
     </div>
   );
